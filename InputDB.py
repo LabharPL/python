3c5887d6e9d394f18021cfdba30d2bd41feec872
AH <arekh@o2.pl>
2019-09-06 10:44:21 +0200

    mysql_connection

---
+++ InputDB.py
@@ -0,0 +1,45 @@
+#  <?php
+# $servername = "s34.linuxpl.com";
+# $username = "arekh_mqtt";
+# $password = "Lublewo@12";
+# $dbname = "arekh_mqtt";
+#
+# // Create connection
+# $conn = new mysqli($servername, $username, $password, $dbname);
+# // Check connection
+# if ($conn->connect_error) {
+#     die("Connection failed: " . $conn->connect_error);
+# }
+#
+# $sql = "INSERT INTO test (Field, timnestamp)
+# VALUES ('2', '12'')";
+#
+# if ($conn->query($sql) === TRUE) {
+#     echo "New record created successfully";
+# } else {
+#     echo "Error: " . $sql . "<br>" . $conn->error;
+# }
+#
+# $conn->close();
+# ?>
+
+
+
+import mysql.connector
+
+mydb = mysql.connector.connect(
+  host="s34.linuxpl.com",
+  user="arekh_mqtt",
+  passwd="Lublewo@12",
+  database="arekh_mqtt"
+)
+
+mycursor = mydb.cursor()
+
+sql = "INSERT INTO test (Field, timnestamp) VALUES (%s, %s)"
+val = ("2", "21")
+mycursor.execute(sql, val)
+
+mydb.commit()
+
+print(mycursor.rowcount, "record inserted.")
