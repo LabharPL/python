1075626da1737d84b41cca2ffc8a959d3a250dec
labhar <labhar@o2.pl>
2019-09-09 19:02:07 +0200

    examples added

--- short_example.py
+++ short_example.py
@@ -43,4 +43,17 @@ print(my_function(9))
 def my_function(child3, child2, child1):
   print("The youngest child is " + child3)
 
-my_function(child1 = "Emil", child2 = "Tobias", child3 = "Linus")
+my_function(child1 = "Emil", child2 = "Tobias", child3 = "Linusek")
+
+
+
+def tri_recursion(k):
+  if(k>0):
+    result = k+tri_recursion(k-1)
+    print(result)
+  else:
+    result = 0
+  return result
+
+print("\n\nRecursion Example Results")
+tri_recursion(646)
