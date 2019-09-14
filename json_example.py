24218a51d27bdde37d0f63f3ea49d1da1dc95556
labhar <labhar@o2.pl>
2019-09-14 20:42:10 +0200

    examples json/response

--- json_example.py
+++ json_example.py
@@ -28,13 +28,44 @@
 # str = "The rain in Spain"
 # x = re.findall("ai", str)
 # print(x)
+#
+# import requests
+# r = requests.get('https://api.github.com')
+# # <Response [200]>
+# print(r)
+#
+# if r.status_code == 200:
+#     print('Success!')
+# elif r.status_code == 404:
+#     print('Not Found.')
 
-import requests
-r = requests.get('https://api.github.com')
-# <Response [200]>
-print(r)
 
-if r.status_code == 200:
-    print('Success!')
-elif r.status_code == 404:
-    print('Not Found.')
+
+# import requests
+# from requests.exceptions import HTTPError
+#
+# for url in ['https://api.github.com', 'https://api.github.com/invalid']:
+#     try:
+#         response = requests.get(url)
+#
+#         # If the response was successful, no Exception will be raised
+#         response.raise_for_status()
+#     except HTTPError as http_err:
+#         print(f'HTTP error occurred: {http_err}')  # Python 3.6
+#     except Exception as err:
+#         print(f'Other error occurred: {err}')  # Python 3.6
+#     else:
+#         print('Success!')
+
+
+
+try:
+  f = open("demofile.txt")
+  print('tu jest f', f)
+
+  f.write("Lorum Ipsum")
+except:
+  print("Something went wrong when writing to the file")
+finally:
+  f.close()
+  print("Something else went wrong")
