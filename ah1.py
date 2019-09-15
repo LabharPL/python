2f1572b3b675b4bdb086ff2fca9ba0a033cc784f
labhar <labhar@o2.pl>
2019-09-15 12:01:29 +0200

    examples json/response

--- ah1.py
+++ ah1.py
@@ -3,3 +3,51 @@ person1 = {
   "age": 36,
   "country": "Norway"
 }
+
+
+
+
+# thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+# print(thislist)
+# thislist[1] = "blackcurrant"
+# print(thislist)
+# thisset = {"apple", "banana", "cherry"}
+# print(thisset)
+#
+# a = 33
+# b = 200
+# if b > a:
+#   print("b is greater than a")
+#
+# a = 33
+# b = 33
+# if b > a:
+#   print("b is greater than a")
+# elif a == b:
+#   print("a and b are equal")
+#
+# a = 200
+# b = 33
+# if b > a:
+#     print("b is greater than a")
+# elif a == b:
+#     print("a and b are equal")
+# else:
+#     print("a is greater than b")
+#
+#
+#
+# # a = 2
+# # b = 330
+# #
+# # print("A") if a > b else print("B")
+#
+#
+#
+# cars = ["Ford", "Volvo", "BMW"]
+#
+# print(cars)
+#
+# x = len(cars)
+# print ('ilosc');
+# print( x)
